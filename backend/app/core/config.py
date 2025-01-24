e4fef2ff05d4026d1431de79202e9382f2c0115e
Addisu <addisuhaile87@gmail.com>
2025-01-24 17:23:16 +0300

    Update config file

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,33 +1,53 @@
-import os
+from functools import lru_cache
 
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Config:
-    SECRET_KY = os.getenv("SECRET_KEY")
-    API_PUBLIC_URL = os.getenv("API_PUBLIC_URL")
+class BaseConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+    ENV_STATE: str | None = None
+
+
+# Define the configuration settings class
+class GlobalConfig(BaseSettings):
+    DATABASE_URL: str | None = None
+    DB_FORCE_ROLL_BACK: bool = False
 
     # Twilio API Credentials
-    TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
-    TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
-    TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
+    TWILIO_ACCOUNT_SID: str | None = None
+    TWILIO_AUTH_TOKEN: str | None = None
+    TWILIO_PHONE_NUMBER: str | None = None
 
     # OpenAI API Credentials
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    OPENAI_API_KEY: str | None = None
 
     # Eleven Labs API Credentials
-    ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
-    VOICE_ID = os.getenv("VOICE_ID")
+    ELEVENLABS_API_KEY: str | None = None
+    VOICE_ID: str | None = None
 
-    REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-    REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 
-    # Company Details
-    COMPANY_NAME = os.getenv("COMPANY_NAME")
-    COMPANY_BUSINESS = os.getenv("COMPANY_BUSINESS")
-    CONVERSATION_PURPOSE = os.getenv("CONVERSATION_PURPOSE")
-    COMPANY_PRODUCTS_SERVICES = os.getenv("COMPANY_PRODUCTS_SERVICES")
-    AISALESAGENT_NAME = os.getenv("AISALESAGENT_NAME")
+class DevConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="DEV_", extra="ignore")
+
+
+class ProdConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="PROD_", extra="ignore")
+
+
+class TestConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="TEST_", extra="ignore")
+
+    DATABASE_URL: str = "sqlite:///./test.db"
+    DB_FORCE_ROLL_BACK: bool = True
+
+
+# Use lru_cache to cache the configuration object
+@lru_cache
+def get_config(env_state: str):
+    configs = {"dev": DevConfig, "prod": ProdConfig, "test": TestConfig}
+    return configs[env_state]()
+
+
+# Accessing the configuration
+config = get_config(BaseConfig().ENV_STATE)
